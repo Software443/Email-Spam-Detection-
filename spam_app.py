5944ab51019c0033659c60afaa136e9c8ca046ef
Gabriel Simeon <gabrielsimeon443@gmail.com>
2025-08-05 23:38:46 +0100

    First commit - committing all my files of the email spam to the repository

---
+++ spam_app.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import pickle
+import streamlit as st
+
+
+st.title("📩Spam Email Classification App")
+
+st.write("""This app classifies emails as Spam or Not Spam using a pre-trained model. 
+It uses a Logistic Regression model trained on a dataset of emails.""")
+# -----> load the model and vectorizer
+# If You Used a CountVectorizer or TfidfVectorizer Before Training:
+# You must transform the new input using the same vectorizer before passing it to the model.
+with open(r'C:\Users\Hp\Logistic_model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+with open(r'C:\Users\Hp\cv_model.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+
+# Sample input (ensure it's string)
+user_input = str(st.text_area("Enter email text here"))
+
+# Transform before prediction
+if st.button("Predict"):
+    st.write("Processing your input...")
+    if user_input.strip() != "":
+        input_transformed = vectorizer.transform([user_input])  # now numeric format
+        prediction = loaded_model.predict(input_transformed)[0]
+
+        if prediction == 1:
+            st.error("🚨 This is a Spam message!")
+        else:
+            st.success("✅ This is Not Spam.")
+    else:
+        st.warning("Please enter some text.")
